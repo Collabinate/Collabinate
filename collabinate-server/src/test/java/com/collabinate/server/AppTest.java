4147ff4e8c41a1f732e54cecc71af946ab296a58
Jack Jones <github@uberjack.com>
2012-06-07 23:07:20 -0400

    Update test for junit 4

--- collabinate-server/src/test/java/com/collabinate/server/AppTest.java
+++ collabinate-server/src/test/java/com/collabinate/server/AppTest.java
@@ -1,38 +1,17 @@
 package com.collabinate.server;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+	@Test
     public void testApp()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
 }
