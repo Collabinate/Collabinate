ac1e07f50b5dc12c8e40703be6d987d92300a5a8
Jack Jones <github@uberjack.com>
2012-06-12 00:46:10 -0400

    Playing with tests

--- collabinate-server/src/test/java/com/collabinate/server/GraphDbTest.java
+++ collabinate-server/src/test/java/com/collabinate/server/GraphDbTest.java
@@ -2,9 +2,12 @@ package com.collabinate.server;
 
 import static org.junit.Assert.*;
 
+import java.awt.Graphics2D;
+
 import org.junit.Test;
 
 import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 
 public class GraphDbTest
@@ -13,10 +16,11 @@ public class GraphDbTest
 	public void shouldAttachToExistingDatabase()
 	{
 		Graph g = new TinkerGraph();
-		g.addVertex(null);
+		Vertex v = g.addVertex(null);
 		GraphDb.attach(g);
 		Graph graph = GraphDb.getGraph();
 		assertTrue(graph.getVertices().iterator().hasNext());
+		assertEquals(v, graph.getVertices().iterator().next());
 	}
 
 }
