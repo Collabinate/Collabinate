5cb446c53ea20f47d0cc83e898853dce956502cc
Jack Jones <github@uberjack.com>
2013-07-22 20:04:28 -0400

    More documentation
    Javadoc for the core server interfaces

--- collabinate-server/src/main/java/com/collabinate/server/CollabinateReader.java
+++ collabinate-server/src/main/java/com/collabinate/server/CollabinateReader.java
@@ -2,9 +2,33 @@ package com.collabinate.server;
 
 import java.util.List;
 
+/**
+ * The interface for a Collabinate server that performs read (retrieval)
+ * operations.
+ * 
+ * @author mafuba
+ *
+ */
 public interface CollabinateReader
 {
+	/**
+	 * Retrieves a collection of entries for an entity, with paging ability. 
+	 * 
+	 * @param entityId The ID of the entity for which to retrieve a stream.
+	 * @param startIndex The zero-based index of the first element to retrieve.
+	 * @param itemsToReturn The maximum number of stream entries to retrieve.
+	 * @return A collection of stream entries for the given entity.
+	 */
 	public List<StreamItemData> getStream(String entityId, long startIndex, int itemsToReturn);
 	
+	/**
+	 * Retrieves a collection of stream entries for the entities that a user
+	 * follows, in chronological order.
+	 * 
+	 * @param userId The ID of the user for which to retrieve a feed.
+	 * @param startIndex The zero-based index of the first element to retrieve.
+	 * @param itemsToReturn The maximum number of feed entries to retrieve.
+	 * @return A collection of feed entries for the given user.
+	 */
 	public List<StreamItemData> getFeed(String userId, long startIndex, int itemsToReturn);
 }
--- collabinate-server/src/main/java/com/collabinate/server/CollabinateWriter.java
+++ collabinate-server/src/main/java/com/collabinate/server/CollabinateWriter.java
@@ -1,8 +1,28 @@
 package com.collabinate.server;
 
+/**
+ * The interface for a Collabinate server that performs write (update)
+ * operations.
+ * 
+ * @author mafuba
+ *
+ */
 public interface CollabinateWriter
 {
+	/**
+	 * Adds an entry to an entity's stream, at the correct chronological
+	 * location.
+	 * 
+	 * @param entityId The entity for which an entry will be added.
+	 * @param streamEntry The entry to add.
+	 */
 	public void addStreamItem(String entityId, StreamItemData streamItem);
-		
+	
+	/**
+	 * Adds an entity to the collection of entities a user follows.
+	 * 
+	 * @param userId The ID of the user that follows the entity.
+	 * @param entityId The ID of the entity that the user follows.
+	 */
 	public void followEntity(String userId, String entityId);
 }
