08cd31bc5c43e555710648ab154db6260b05748c
Jack Jones <github@uberjack.com>
2013-08-10 10:04:08 -0400

    Start implementing REST

---
+++ collabinate-server/src/main/java/com/collabinate/server/CollabinateApplication.java
@@ -0,0 +1,21 @@
+package com.collabinate.server;
+
+import org.restlet.Application;
+
+/**
+ * Main Restlet application
+ * 
+ * @author mafuba
+ *
+ */
+public class CollabinateApplication extends Application
+{
+
+	/**
+	 * Sets the application properties.
+	 */
+	public CollabinateApplication()
+	{
+		setName("Collabinate");
+	}
+}
---
+++ collabinate-server/src/test/java/com/collabinate/server/CollabinateApplicationTest.java
@@ -0,0 +1,22 @@
+package com.collabinate.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.restlet.Application;
+
+/**
+ * Test class for the server application.
+ * 
+ * @author mafuba
+ *
+ */
+public class CollabinateApplicationTest
+{	
+	@Test
+	public void application_should_be_named_collabinate()
+	{
+		Application app = new CollabinateApplication();
+		assertTrue("Collabinate" == app.getName());
+	}
+}
