54fa4b894ecb2035341fe4492c4bbd8d1b60b61a
Jack Jones <github@uberjack.com>
2013-08-14 22:07:12 -0400

    Add test for invalid route
    Clean up component test class and add a failing test for a bad route.

--- collabinate-server/src/test/java/com/collabinate/server/CollabinateComponentTest.java
+++ collabinate-server/src/test/java/com/collabinate/server/CollabinateComponentTest.java
@@ -2,6 +2,8 @@ package com.collabinate.server;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.restlet.Component;
 import org.restlet.Request;
@@ -16,20 +18,45 @@ import org.restlet.data.Method;
  */
 public class CollabinateComponentTest
 {
+	private Component component;
+	
+	@Before
+	public void Setup()
+	{
+		component = new CollabinateComponent(null, null);
+	}
+	
+	@After
+	public void Teardown() throws Exception
+	{
+		if (component.isStarted())
+		{
+			component.stop();
+		}
+	}
+	
 	@Test
 	public void application_should_be_named_collabinate()
 	{
-		Component component = new CollabinateComponent(null, null);
 		assertEquals("Collabinate", component.getName());
 	}
 	
 	@Test
 	public void getting_root_resource_should_return_200()
 	{
-		Component component = new CollabinateComponent(null, null);
 		Request request = new Request(Method.GET, "riap://application/");
 		Response response = component.handle(request);
 		
 		assertEquals(200, response.getStatus().getCode());
+	}
+	
+	@Test
+	public void getting_invalid_route_should_return_500()
+	{
+		Request request = new Request(Method.GET, 
+				"riap://application/invalid/route");
+		Response response = component.handle(request);
+		
+		assertEquals(404, response.getStatus().getCode());		
 	}
 }
