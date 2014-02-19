80749efc40d10691dc1d53e4648a7b4e666eaa97
Jack Jones <github@uberjack.com>
2014-02-19 15:12:51 -0500

    Add Batch Follow

--- collabinate-server/src/main/java/com/collabinate/server/resources/FollowingResource.java
+++ collabinate-server/src/main/java/com/collabinate/server/resources/FollowingResource.java
@@ -1,9 +1,15 @@
 package com.collabinate.server.resources;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
+import com.collabinate.server.activitystreams.ActivityStreamsCollection;
+import com.collabinate.server.activitystreams.ActivityStreamsObject;
 import com.collabinate.server.engine.CollabinateReader;
+import com.collabinate.server.engine.CollabinateWriter;;
 
 /**
  * Restful resource representing the entities followed by a user.
@@ -26,9 +32,31 @@ public class FollowingResource extends ServerResource
 		int skip = null == skipString ? 0 : Integer.parseInt(skipString);
 		int take = null == takeString ? DEFAULT_TAKE : 
 			Integer.parseInt(takeString);
-
-
+		
 		return reader.getFollowing(tenantId, userId, skip, take).toString();
+	}
+	
+	@Post
+	public void addFollowing(String collectionString)
+	{
+		// extract necessary information from the context
+		CollabinateWriter writer = (CollabinateWriter)getContext()
+				.getAttributes().get("collabinateWriter");
+		String tenantId = getAttribute("tenantId");
+		String userId = getAttribute("userId");
+
+		ActivityStreamsCollection followingCollection =
+				new ActivityStreamsCollection(collectionString);
+		
+		for (ActivityStreamsObject following : followingCollection.getItems())
+		{
+			String id = following.getId();
+			if (null != id && !id.equals(""))
+			{
+				writer.followEntity(tenantId, userId, id,
+						DateTime.now(DateTimeZone.UTC));
+			}
+		}
 	}
 	
 	private static final int DEFAULT_TAKE = 20;
--- collabinate-server/src/test/java/com/collabinate/server/resources/FollowingResourceTest.java
+++ collabinate-server/src/test/java/com/collabinate/server/resources/FollowingResourceTest.java
@@ -7,9 +7,12 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.restlet.Request;
+import org.restlet.data.MediaType;
 import org.restlet.data.Method;
+import org.restlet.data.Status;
 
 import com.collabinate.server.activitystreams.ActivityStreamsCollection;
+import com.collabinate.server.activitystreams.ActivityStreamsObject;
 
 /**
  * Tests for the Following Resource
@@ -44,8 +47,7 @@ public class FollowingResourceTest extends GraphResourceTest
 				"riap://application/1/tenant/users/user/following/entity");
 		component.handle(request);
 		
-		assertThat(get().getEntityAsText(),
-				not(containsString("tenant/entity")));
+		assertThat(get().getEntityAsText(), not(containsString("tenant")));
 	}
 	
 	@Test
@@ -120,6 +122,83 @@ public class FollowingResourceTest extends GraphResourceTest
 				new ActivityStreamsCollection(get().getEntityAsText());
 		
 		assertEquals(1, following.getTotalItems());
+	}
+	
+	@Test
+	public void empty_post_should_return_200()
+	{
+		assertEquals(Status.SUCCESS_OK, post().getStatus());
+	}
+	
+	@Test
+	public void post_of_empty_collection_should_return_200()
+	{
+		ActivityStreamsCollection collection = new ActivityStreamsCollection();
+		assertEquals(Status.SUCCESS_OK,
+				post(collection.toString(), MediaType.APPLICATION_JSON)
+				.getStatus());
+	}
+	
+	@Test
+	public void post_without_collection_should_not_affect_following_count()
+	{
+		ActivityStreamsCollection originalFollowing =
+				new ActivityStreamsCollection(get().getEntityAsText());
+		
+		post((new ActivityStreamsCollection("foo")).toString(),
+				MediaType.APPLICATION_JSON);
+		
+		ActivityStreamsCollection newFollowing =
+				new ActivityStreamsCollection(get().getEntityAsText());		
+		
+		assertEquals(originalFollowing.size(), newFollowing.size());
+	}
+	
+	@Test
+	public void post_with_new_following_should_increase_following_count()
+	{
+		ActivityStreamsCollection originalFollowing =
+				new ActivityStreamsCollection(get().getEntityAsText());
+		
+		ActivityStreamsCollection addFollowing =
+				new ActivityStreamsCollection();
+		
+		ActivityStreamsObject object = new ActivityStreamsObject();
+		object.setId("foo");
+		
+		addFollowing.add(object);
+		
+		post(addFollowing.toString(), MediaType.APPLICATION_JSON);
+		
+		ActivityStreamsCollection newFollowing =
+				new ActivityStreamsCollection(get().getEntityAsText());		
+		
+		assertEquals(originalFollowing.size() + addFollowing.size(),
+				newFollowing.size());
+	}
+	
+	@Test
+	public void post_with_existing_following_should_not_affect_following_count()
+	{		
+		ActivityStreamsCollection addFollowing =
+				new ActivityStreamsCollection();
+		
+		ActivityStreamsObject object = new ActivityStreamsObject();
+		object.setId("foo");
+		
+		addFollowing.add(object);
+		
+		post(addFollowing.toString(), MediaType.APPLICATION_JSON);
+		
+		ActivityStreamsCollection originalFollowing =
+				new ActivityStreamsCollection(get().getEntityAsText());
+
+		post(addFollowing.toString(), MediaType.APPLICATION_JSON);
+
+		ActivityStreamsCollection newFollowing =
+				new ActivityStreamsCollection(get().getEntityAsText());		
+		
+		assertEquals(originalFollowing.size(), newFollowing.size());
 	}
 	
 	@Override
